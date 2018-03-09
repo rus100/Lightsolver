126f1e579e5deedabdcee2c4b27309c1bf0d94b3
Руслан Ахметов <rus100@yandex.ru>
2018-03-09 23:11:05 +0500

    Create Data.cs

---
+++ Data.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LightSolver
+{
+    class Data
+    {
+        public static double n;//число ламп
+        public static double F;//световой поток
+        public static double mnojit;//множитель
+        public static double polyarangle1;//количество полярных углов
+        public static double azimutangle1;//количество азимутальных углов
+        public static double fotometr;//тип фотометрии
+        public static double systemedin;//система единиц
+        public static double shirin;//ширина светильника
+        public static double dlina;//длина светильника
+     public static double visota;//высота светильника
+     public static double balast;//коэффициент баласта
+     public static double versia;//версия
+     public static double p;//мощность активная светильника
+     public static double[] polyarangle;//список полярных углов
+     public static double[] azimutangle;//список азимутальных углов
+     public static double[,] kss;//КСС
+     public static double a;//длинна помещения
+     public static double b;//ширина помещения
+     public static double h1;//высота помещения
+     public static double l;//длинна подвеса
+     public static double hr;//высота рабочей поверхности
+     public static decimal kz;   
+    
+    }
+}
